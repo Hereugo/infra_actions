a318642f079d5abfb82beeb060d27ccdacfdf8ab
Амир Нурмухамбетов <thereug6@gmail.com>
2023-03-03 14:55:50 +0600

    Testing

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!', status=200)
+    return HttpResponse('У меня получилось запустить код через докер!', status=200)
 
 
 def second_page(request):
